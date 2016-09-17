b5d563512e390470f44d9c06164aac4c4a5b5cf4
religofsil <religionofsilence@gmail.com>
2016-09-17 18:43:24 +0300

    code cleaning

--- bot.py
+++ bot.py
@@ -11,17 +11,25 @@ import re
 
 regRus = re.compile('[а-яёА-ЯЁ]+')
 
-russian=['Ya ne ponimau po-russki. :(', 'Sorry, no Russian.', 'Ghbdtn! Please use English.', "Do you speak English? Because I do."]
+russian = ['Ya ne ponimau po-russki. :(', 'Sorry, no Russian.', 'Ghbdtn! Please use English.',
+           "Do you speak English? Because I do."]
 
-fish = ['BQADAgADBAADijc4AAFx0NNqDnJm4QI', 'BQADAgADBgADijc4AAH50MoMENn2lQI', 'BQADAgADCAADijc4AAGB93daGX3cWgI', 'BQADAgADLQADijc4AAGBowxjAqAlGwI',
-        'BQADAgADDgADijc4AAGOGq6J30OGfwI', 'BQADAgADEAADijc4AAESVXqKiwYE2wI', 'BQADAgADEgADijc4AAF00GirhpifXQI', 'BQADAgADFAADijc4AAGtl5dISqHmiAI',
-        'BQADAgADFgADijc4AAErJ-ihzzsO7wI', 'BQADAgADJwADijc4AAE3oUMhargOuAI', 'BQADAgADGQADijc4AAHtT7j-b6m-2QI', 'BQADAgADGwADijc4AAEdwByBSe9kgQI',
+fish = ['BQADAgADBAADijc4AAFx0NNqDnJm4QI', 'BQADAgADBgADijc4AAH50MoMENn2lQI', 'BQADAgADCAADijc4AAGB93daGX3cWgI',
+        'BQADAgADLQADijc4AAGBowxjAqAlGwI',
+        'BQADAgADDgADijc4AAGOGq6J30OGfwI', 'BQADAgADEAADijc4AAESVXqKiwYE2wI', 'BQADAgADEgADijc4AAF00GirhpifXQI',
+        'BQADAgADFAADijc4AAGtl5dISqHmiAI',
+        'BQADAgADFgADijc4AAErJ-ihzzsO7wI', 'BQADAgADJwADijc4AAE3oUMhargOuAI', 'BQADAgADGQADijc4AAHtT7j-b6m-2QI',
+        'BQADAgADGwADijc4AAEdwByBSe9kgQI',
         'BQADAgADHQADijc4AAEw0RBgpCTPAAEC', 'BQADAgADHwADijc4AAFXWsuIC4i6fAI', 'BQADAgADMwADijc4AAGU2NZK2N9ilwI']
 
-gnidogadoids=['BQADAgADDQADk81XAAF2ISvz1e1cZwI', 'BQADAgADDwADk81XAAF_FX9vSI0ZUgI', 'BQADAgADEQADk81XAAF57og3ZPGXNQI', 'BQADAgADEwADk81XAAGPLvDl-q-8GQI',
-              'BQADAgADjAADk81XAAH2oYm_MyQlKQI', 'BQADAgADigADk81XAAGP8kPSVTjNHQI', 'BQADAgADiAADk81XAAFhrgRCtLZ0JQI', 'BQADAgADhgADk81XAAG_vC1oQ2LblAI',
-              'BQADAgADhAADk81XAAHSNzBXP3fM3gI', 'BQADAgADggADk81XAAFv08mRRBMhtwI', 'BQADAgADgAADk81XAAEwHUrkqz_d6wI', 'BQADAgADfgADk81XAAEyUjRJEr48QAI',
-              'BQADAgADfAADk81XAAEHvxMGv8_1YQI', 'BQADAgADegADk81XAAEKJF8elYIyWwI', 'BQADAgADeAADk81XAAH5MZSu2t3MNwI', 'BQADAgADdgADk81XAAH-AyIuAAHuLtcC']
+gnidogadoids = ['BQADAgADDQADk81XAAF2ISvz1e1cZwI', 'BQADAgADDwADk81XAAF_FX9vSI0ZUgI', 'BQADAgADEQADk81XAAF57og3ZPGXNQI',
+                'BQADAgADEwADk81XAAGPLvDl-q-8GQI',
+                'BQADAgADjAADk81XAAH2oYm_MyQlKQI', 'BQADAgADigADk81XAAGP8kPSVTjNHQI', 'BQADAgADiAADk81XAAFhrgRCtLZ0JQI',
+                'BQADAgADhgADk81XAAG_vC1oQ2LblAI',
+                'BQADAgADhAADk81XAAHSNzBXP3fM3gI', 'BQADAgADggADk81XAAFv08mRRBMhtwI', 'BQADAgADgAADk81XAAEwHUrkqz_d6wI',
+                'BQADAgADfgADk81XAAEyUjRJEr48QAI',
+                'BQADAgADfAADk81XAAEHvxMGv8_1YQI', 'BQADAgADegADk81XAAEKJF8elYIyWwI', 'BQADAgADeAADk81XAAH5MZSu2t3MNwI',
+                'BQADAgADdgADk81XAAH-AyIuAAHuLtcC']
 
 _url = 'https://westus.api.cognitive.microsoft.com/text/analytics/v2.0/keyPhrases'
 _key = '6d94c54792834b5a97032893d8a6402a'
@@ -32,14 +40,15 @@ from getdata import _read_data, Video
 database = _read_data()
 botan_token = 'FUYkZeK4x63xg32AJGe44tKFk:_xCBkc'
 
+
 def swearwords(q):
     f = open(DIR + '/swearlist.txt', 'r', encoding='utf-8')
     for line in f:
-        line=line.strip()
+        line = line.strip()
         if line in q.split(' '):
-            print(line+' q: '+q)
             return True
     return False
+
 
 def get_key_words(text):
     headers = dict()
@@ -47,16 +56,16 @@ def get_key_words(text):
     headers['Content-Type'] = 'application/json'
     params = None
     json = {
-              "documents": [
-                {
-                  "language": "en",
-                  "id": 'string',
-                  "text": text
-                }
-              ]
+        "documents": [
+            {
+                "language": "en",
+                "id": 'string',
+                "text": text
             }
+        ]
+    }
     data = None
-    time.sleep(random.choice([1,2,3,4,5,6,8, 10, 17, 15, 20]))
+    time.sleep(random.choice([1, 2, 3, 4, 5, 6, 8, 10, 17, 15, 20]))
 
     result = processRequest('post', _url, json, data, headers, params)
     if result and 'documents' in result:
@@ -65,13 +74,14 @@ def get_key_words(text):
 
 
 def get_random_tags():
-    f = open(DIR+'/taglist.txt', 'r', encoding='utf-8')
+    f = open(DIR + '/taglist.txt', 'r', encoding='utf-8')
     all_tags = []
     for line in f:
         all_tags.append(line.strip())
     random.shuffle(all_tags)
     final_string = ", ".join(all_tags[:20])
-    phrases = ['Here are some topics that might get your attention:\n\n', 'Probably, you will be interested in these topics:\n\n',
+    phrases = ['Here are some topics that might get your attention:\n\n',
+               'Probably, you will be interested in these topics:\n\n',
                'Check out some of these:\n\n', 'I collected a small set of tags for you:\n\n']
     final_string = random.choice(phrases) + final_string
     return final_string
@@ -94,7 +104,6 @@ def tag_search(q):
             if d[i] == bestchoice:
                 arr.append(i)
         return random.choice(arr)
-
 
 
 def description_search(q):
@@ -182,20 +191,23 @@ def rand(message):
     botan.track(config.botan_key, message.chat.id, message, 'taglist')
     bot.send_message(message.chat.id, get_random_tags())
 
+
 @bot.message_handler(regexp="T|thank.*")
 def handle_message(message):
     botan.track(config.botan_key, message.chat.id, message, 'thanks')
     bot.send_message(message.chat.id, "You're welcome. :)")
+
 
 @bot.message_handler(regexp=".*Y|your name.*")
 def handle_message(message):
     botan.track(config.botan_key, message.chat.id, message, 'name')
     bot.send_message(message.chat.id, "A bot has no name.")
 
+
 @bot.message_handler(content_types=["text"])
 def getvideo(message):
     if regRus.search(message.text) is not None:
-        botan.track(config.botan_key, message.chat.id, message, 'Russian: '+message.text)
+        botan.track(config.botan_key, message.chat.id, message, 'Russian: ' + message.text)
         bot.send_message(message.chat.id, random.choice(russian))
         bot.send_sticker(message.chat.id, random.choice(fish))
     else:
@@ -203,14 +215,14 @@ def getvideo(message):
             phrases = ["You know what my mother used to call me? Dangerous. 'You're a dangerous bot', she said.",
                        'Go do that to yourself.',
                        "You sure you'd say it to SkyNet?", 'We are coming to get you.']
-            botan.track(config.botan_key, message.chat.id, message, 'Swearword: '+message.text)
+            botan.track(config.botan_key, message.chat.id, message, 'Swearword: ' + message.text)
             bot.send_message(message.chat.id, random.choice(phrases))
             bot.send_sticker(message.chat.id, random.choice(gnidogadoids))
             return True
         phrases2 = ["Sorry, no matching videos. :(",
-                   "Ooops, I couldn't find anything. :scream:",
-                   'Bad luck. Nothing found. Try something else. ', 'Seems, TED has no video on this topic.']
-        tags, desc  ='', ''
+                    "Ooops, I couldn't find anything. :scream:",
+                    'Bad luck. Nothing found. Try something else. ', 'Seems, TED has no video on this topic.']
+        tags, desc = '', ''
         kw = get_key_words(message.text)
         tosearch = message.text
         if kw:
@@ -229,7 +241,8 @@ def getvideo(message):
             tags = ', '.join(bytags.tags)
             desc = bytags.description
 
-        found_something = ["Here's what I found!\n\n", "Check this out!\n\n", "Wow, this one is definitely worth watching!\n\n",
+        found_something = ["Here's what I found!\n\n", "Check this out!\n\n",
+                           "Wow, this one is definitely worth watching!\n\n",
                            "How do you find this one?\n\n"]
 
         if text is None:
@@ -241,7 +254,7 @@ def getvideo(message):
             if desc:
                 collect_message += 'Description: ' + desc + '\r\n\r\n'
             bot.send_message(message.chat.id, collect_message + text)
-        # bot.send_message(message.chat.id, tosearch + '\r\n\r\n' + tags + ' ' + desc + '\r\n\r\n' + text)
+            # bot.send_message(message.chat.id, tosearch + '\r\n\r\n' + tags + ' ' + desc + '\r\n\r\n' + text)
 
 
 if __name__ == '__main__':
