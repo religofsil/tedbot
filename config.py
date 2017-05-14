66c95c5e27d2c0d903069fce321e2f9698230da4
Elmira <cclikespizza@gmail.com>
2017-05-14 14:36:25 +0300

    add gitignore

--- config.py
+++ config.py
@@ -1,2 +1,10 @@
-token = '255139696:AAEjxLtMn9sXm5-DaEZ23sqfZldc-npaLKE'
-botan_key = 'd301e3b9-caa4-4cd6-ac88-d5c193f5abc5'
+TOKEN = '255139696:AAEjxLtMn9sXm5-DaEZ23sqfZldc-npaLKE'
+BOTAN_KEY = 'd301e3b9-caa4-4cd6-ac88-d5c193f5abc5'
+BOTAN_TOKEN = 'FUYkZeK4x63xg32AJGe44tKFk:_xCBkc'
+
+DATA = '/data/database.json'
+WORDLIST = '/data/swearlist.txt'
+TAGLIST = '/data/taglist.txt'
+
+KEYWORDS_URL = 'https://westus.api.cognitive.microsoft.com/text/analytics/v2.0/keyPhrases'
+KEYWORDS_KEY = '386d4939e3af43adbee1bb441dba35ad'
