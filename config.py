858dc0c221e091e9b4c312c6f279e534cc6c24a0
Elmira <cclikespizza@gmail.com>
2017-05-14 14:39:52 +0300

    config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
-TOKEN = '255139696:AAEjxLtMn9sXm5-DaEZ23sqfZldc-npaLKE'
-BOTAN_KEY = 'd301e3b9-caa4-4cd6-ac88-d5c193f5abc5'
-BOTAN_TOKEN = 'FUYkZeK4x63xg32AJGe44tKFk:_xCBkc'
+TOKEN = '==='
+BOTAN_KEY = '==='
+BOTAN_TOKEN = '==='
 
 DATA = '/data/database.json'
 WORDLIST = '/data/swearlist.txt'
 TAGLIST = '/data/taglist.txt'
 
 KEYWORDS_URL = 'https://westus.api.cognitive.microsoft.com/text/analytics/v2.0/keyPhrases'
-KEYWORDS_KEY = '386d4939e3af43adbee1bb441dba35ad'
+KEYWORDS_KEY = '==='
